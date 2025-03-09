1f1055602654a2f325154fe4c2ecf258c7bf2227
Arthur Baudin <89687104+dd-absol@users.noreply.github.com>
2025-03-09 14:38:08 +0100

    added fuller page structure

---
+++ src/types/RecipeData.ts
@@ -0,0 +1,19 @@
+export type { RecipeData }; 
+
+type RecipeData = {
+  id: string;
+  name: string;
+  calories: number;
+  category: string;
+  image_url: string;
+  cook_time: number;
+  cost: number;
+  created_at: string;
+  created_by: string;
+  intructions: string;
+  prep_time: number;
+  published: boolean;
+  servings: number;
+  when_to_eat: string;
+};
+
